f1664c6f0d727a82c73fd279bb9558be3ccb1414
Gabriel Valero <63328736+GabrielValero@users.noreply.github.com>
2022-03-31 18:45:33 -0400

    Cambio del footer
    En vez de un componente de clases lo acomode para que fuese un componente funcional. E implemente el componente FooterElement.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,56 +4,30 @@ import {faLinkedin} from '@fortawesome/free-brands-svg-icons';
 import {faGithub} from '@fortawesome/free-brands-svg-icons';
 import {faWhatsapp} from '@fortawesome/free-brands-svg-icons';
 
-class Footer extends React.Component{
+import FooterElement from './Atoms/FooterElement'
 
-    render(){
-        return (
-                
-                <footer class="footer">
+export default function Footer(){
+    return(
+        <footer class="footer">
             <div class="container footer__grid">
-            <nav class="nav nav--footer">
-                <a class="nav__items nav__items--footer" href="#inicio">Inicio</a>
-                <a class="nav__items nav__items--footer" href="#sobreMi">Sobre mi</a>
-                <a class="nav__items nav__items--footer" href="#skills">Mis skills</a>
-                <a class="nav__items nav__items--footer" href="#proyectos">Proyectos</a>
-            </nav>
+                <nav class="nav nav--footer">
+                    <a class="nav__items nav__items--footer" href="#inicio">Inicio</a>
+                    <a class="nav__items nav__items--footer" href="#sobreMi">Sobre mi</a>
+                    <a class="nav__items nav__items--footer" href="#skills">Mis skills</a>
+                    <a class="nav__items nav__items--footer" href="#proyectos">Proyectos</a>
+                </nav>
 
-            <section className="footer__contact">
-                <h3 className="footer__title">Contactame</h3>
-                <div className="footer__icons">
-                    <span className="footer__container-icons">
-                        <a 
-                        className="icons"
-                        href="https://www.linkedin.com/in/florencia-talavera-1ba78a22a/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        ><FontAwesomeIcon icon={faLinkedin} className="footer__icon"/>
-                        </a>
-                    </span>
-                    <span className="footer__container-icons">
-                        <a 
-                        className="icons" 
-                        href="https://github.com/flortalavera?tab=repositories"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        ><FontAwesomeIcon icon={faGithub} className="footer__icon"/>
-                        </a>
-                    </span>
-                    <span className="footer__container-icons">
-                        <a className="icons"
-                        href="http://wa.me/541131362662"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        ><FontAwesomeIcon icon={faWhatsapp} className="footer__icon"/>
-                        </a>
-                    </span>
-                </div>
-            </section>
-            <p className="copy">&copy; Mi Portfolio en React by Flor Talavera </p>
-        </div>
-    </footer>
-        );
-    }
+                <section className="footer__contact">
+                    <h3 className="footer__title">Contactame</h3>
+                    <div className="footer__icons">
+                        <FooterElement link={"https://www.linkedin.com/in/florencia-talavera-1ba78a22a/"} Icon={()=><FontAwesomeIcon icon={faLinkedin} className="footer__icon"/>}/>
+                        <FooterElement link={"https://github.com/flortalavera?tab=repositories"} Icon={()=><FontAwesomeIcon icon={faGithub} className="footer__icon"/>}/>
+                        <FooterElement link={"http://wa.me/541131362662"} Icon={()=><FontAwesomeIcon icon={faWhatsapp} className="footer__icon"/>}/>
+                    </div>
+                </section>
+                <p className="copy">&copy; Mi Portfolio en React by Flor Talavera </p>
+            </div>
+        </footer>
+    );
+    
 }
-
-export default Footer;
